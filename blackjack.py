8d584c86733ab02741c7f2e585cef79a59b9dfc5
Jaylan Wilkins <jay.wilkins23@yahoo.com>
2023-08-15 11:33:19 -0400

    added a deck of cards

--- blackjack.py
+++ blackjack.py
@@ -1,1 +1,31 @@
-# Write your blackjack game here.
+import random
+
+SUITS = ['❤', '♦', '♠', '♣']
+RANKS = ['A', 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K']
+
+
+class Deck:
+    def __init__(self, suits, ranks):
+        self.cards = []
+        for suit in suits:
+            for rank in ranks:
+                card = Card(suit, rank)
+                self.cards.append(card)
+
+
+class Card:
+    def __init__(self, suit, rank):
+        self.suit = suit
+        self.rank = rank
+
+    def __str__(self):
+        return f'{self.suit} {self.rank}'
+
+
+new_deck = Deck(SUITS, RANKS)
+for card in new_deck.cards:
+    print(card)
+
+
+# user_card = Card(K, '❤')
+# print(user_card)
